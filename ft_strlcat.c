210eed6ce37d78c271b962b088eb76fa7c96df8f
Zain MATHEWS <zmathews@c4r21s5.wethinkcode.co.za>
2019-05-23 17:56:12 +0200

    explain strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: zmathews <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 08:42:37 by zmathews          #+#    #+#             */
-/*   Updated: 2019/05/23 09:19:56 by zmathews         ###   ########.fr       */
+/*   Updated: 2019/05/23 17:51:11 by zmathews         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 size_t	strlcat(char * dst, const char * src, size_t dstsize)
 {
-	
+	size_t i;
+	size_t j;
+	size_t p;									// may not be needed
+
+	i = 0;
+	j = 0;
+	p = ft_strlen(dst);							// ^
+	while (dst[i] &&  i < dstsize)
+	{
+		i++;
+	}
+	while (i + j < dstsize - 1 && src[j])
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i + j < dstsize)
+	{
+		dst[i + j] = '\0';
+		return (ft_strlen(src) + 2);
+	}
+	else
+		return (0);
 }
