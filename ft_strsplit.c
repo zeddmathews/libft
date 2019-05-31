1f1c93ebdb2be9a7d46fa192166466925582266f
Zain MATHEWS <zmathews@c4r18s5.wethinkcode.co.za>
2019-05-31 13:28:38 +0200

    Fuck's sakes

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,13 +6,51 @@
 /*   By: zmathews <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 10:41:05 by zmathews          #+#    #+#             */
-/*   Updated: 2019/05/30 10:44:21 by zmathews         ###   ########.fr       */
+/*   Updated: 2019/05/31 10:51:30 by zmathews         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_strcnt(const char *s, char c)
+{
+	unsigned int s_count;
+	
+	s_count = 0;
+	while (*s != '\0')
+	{
+		while (*s != '\0' && *s == c)
+			s++;
+		if (*s != '\0')
+			s_count++;
+		while (*s != '\0' && *s != c)
+			s++;
+	}
+	return (s_count);
+}
+
 char	**ft_strsplit(const char *s, char c)
 {
-	
+	int i;
+	int k;
+	int ai;
+	char *sa;
+	i = 0;
+	ai = 0;
+	if (!s || !(sa = (char **)malloc(sizeof(char *) * (ft_strcnt(s, c) + 1))));
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		b = i;
+		while (s[i] != '\0' && s[i] != c)
+			i++;
+		if (i > b)
+		{
+			sa[ai] = ft_strsub(s, b, i - b);
+			ai++;
+	}
+	sa[ai] = NULL;
+	return (sa);
 }
