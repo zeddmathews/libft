9ccd09cb0ba499c2360e619c7e5f8acfc19953b4
Zain MATHEWS <zmathews@c4r18s4.wethinkcode.co.za>
2019-06-21 07:11:58 +0200

    the fuck

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zmathews <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:59:27 by zmathews          #+#    #+#             */
-/*   Updated: 2019/06/19 15:55:08 by zmathews         ###   ########.fr       */
+/*   Updated: 2019/06/21 06:52:41 by zmathews         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ char	*ft_strncpy(char *dest, const char *src, size_t len);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strchr(char *s, int c);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
@@ -62,14 +62,14 @@ void	ft_strdel(char **as);
 void	ft_strclr(char *s);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f) (unsigned int, char *));
-char	*ft_strmap(const char *s, char (*f)(char));
-char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-int		ft_strequ(const char *s1, const char *s2);
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(const char *s1, const char *s2, size_t n);
-char	*ft_strsub(const char *s, unsigned int start, size_t len);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strtrim(const char *s);
-char	**ft_strsplit(const char *s, char c);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s);
+char	**ft_strsplit(char const *s, char c);
 char	*ft_itoa(int num);
 int		ft_numlen(long num);
 
